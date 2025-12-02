78c835d3b8a9e3d50f31c4bf51bbb96618451c5b
Prajjwal Jadhav <121074770+prajwal868@users.noreply.github.com>
2025-12-02 20:48:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import{Header ,Footer ,Avenger} from "./Shield"
+
+export function App(){
+    return<>
+    <Header/>
+    <Avenger/>
+    <Footer/>
+    </>
+
+
+}
---
+++ Shield.js
@@ -0,0 +1,12 @@
+export function Header(){
+    return <h1>This is Header</h1>
+}
+
+
+export function Avenger(){
+    return <h1>This is Avenger</h1>
+}
+
+export function Footer(){
+    return <h1>This is Footer</h1>
+}
